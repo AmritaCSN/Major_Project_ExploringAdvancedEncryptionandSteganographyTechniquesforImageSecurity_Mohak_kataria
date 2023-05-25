bc3e322fc0893175db3cccd705cc574692637b35
mohak kataria <117853652+mohakkataria007@users.noreply.github.com>
2023-05-25 21:32:02 +0530

    Update des.py

--- src/des.py
+++ src/des.py
@@ -31,18 +31,3 @@ class DESCipher(object):
     def _unpad(s):
         return s[:-ord(s[len(s)-1:])]
 
-# test_object=DESCipher('12345678')
-# print("DES :")
-# print("Data Len\tEnc Time\tDec Time")
-# for i in range(20):
-#     s_time=time.time()
-#     message = 'a'*(2**i)
-#     encrypted_text=test_object.encrypt(message)
-#     dec_time=time.time()-s_time
-
-#     s_time=time.time()
-#     decrypted_text=test_object.decrypt(encrypted_text)
-#     enc_time=time.time()-s_time
-#     # debug
-#     # print(f"Encr Text {encrypted_text} == dec text = {decrypted_text}")
-#     print(f"{len(message)}\t{enc_time}\t\t{dec_time}")

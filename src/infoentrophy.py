19f6ac7e52dbbea86f01aa15897734ae7328d9de
mohak kataria <117853652+mohakkataria007@users.noreply.github.com>
2023-05-27 10:52:55 +0530

    Update infoentrophy.py
    addded docstring

--- src/infoentrophy.py
+++ src/infoentrophy.py
@@ -2,6 +2,10 @@ import numpy as np
 from PIL import Image
 
 def information_entropy(image_path):
+    '''
+    This method caclulates entropy which gives a measure of Stegno Image,
+    which gives a measure of how secure is the data - higher is better
+    '''
     # Load stego image
     image = Image.open(image_path)
     pixels = np.array(image.convert('L')).ravel()
